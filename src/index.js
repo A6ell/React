66673a2716facb35af96e57274c3fae25248f846
Ab-Ruh <abell.demere@gmail.com>
2023-06-02 22:32:37 +0300

    React Apps

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import BMI from './BMI';
+// import AgeCalendar from './AgeCalendar'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     {/* <BMI /> */}
+     {/* <AgeCalendar /> */}
+     <App />
+   
   </React.StrictMode>
 );
 
